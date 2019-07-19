6d621dc7fe2dffeab03228c95aac38a11e989fc2
Kila2 <277014717@qq.com>
2019-07-19 14:33:04 +0800

    update init_ios.sh

--- init_ios.sh
+++ init_ios.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-if [ -d ".ios" ] || [ -d ".android" ]
+if [ -d "ios" ] || [ -d "android" ]
 then 
   echo 'its a flutter project'
 else
   echo "Not in flutter project"
   exit
 fi
-open -a Simulator
-flutter run -d all
-pod lib create --template-url=https://github.com/Kila2/ios_flutter_template.git $1
-echo $1 > .FlutterModuleNameiOS
+
+echo $1 > .APFShotiOS
